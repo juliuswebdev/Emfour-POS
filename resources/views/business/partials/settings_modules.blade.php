ed02d8ccc4fa25dadec218fad0621e0e374a5cd8
Julius Mateo <partanduls@gmail.com>
2023-06-02 01:50:04 +0800

    remove comment
    remove comment

--- resources/views/business/partials/settings_modules.blade.php
+++ resources/views/business/partials/settings_modules.blade.php
@@ -4,19 +4,6 @@
 		<h4>@lang('lang_v1.enable_disable_modules')</h4>
       <ul>
 		  @foreach($modules as $k => $v)
-            <!-- <div class="col-sm-4">
-                <div class="form-group">
-                    <div class="checkbox">
-                    <br>
-                      <label>
-                        {!! Form::checkbox('enabled_modules[]', $k,  in_array($k, $enabled_modules) , 
-                        ['class' => 'input-icheck']); !!} 
-                        {{$v['name']}}
-                      </label>
-                      @if(!empty($v['tooltip'])) @show_tooltip($v['tooltip']) @endif
-                    </div>
-                </div>
-            </div> -->
             <li><label>{{$v['name']}}</label>@if(!empty($v['tooltip'])) @show_tooltip($v['tooltip']) @endif</li>
         @endforeach
       </ul>
