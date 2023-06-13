72ef959ed9a9a1adf2412045909d27fff5d72ed6
Julius Mateo <partanduls@gmail.com>
2023-06-14 01:08:25 +0800

    up
    up

--- routes/web.php
+++ routes/web.php
@@ -250,6 +250,7 @@ Route::middleware(['setData', 'auth', 'SetSessionData', 'language', 'timezone', 
     Route::get('/sells/pos/get-recent-transactions', [SellPosController::class, 'getRecentTransactions']);
     Route::get('/sells/pos/get-product-suggestion', [SellPosController::class, 'getProductSuggestion']);
     Route::get('/sells/pos/get-featured-products/{location_id}', [SellPosController::class, 'getFeaturedProducts']);
+    Route::get('/sells/pos/get-product-by-filter', [SellPosController::class, 'getProductByFilter']);
     Route::get('/reset-mapping', [SellController::class, 'resetMapping']);
 
     Route::resource('pos', SellPosController::class);
