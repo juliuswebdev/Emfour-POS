0dd658c28675e283fee1e4a501d41437541fea33
Prakash Tank <prakashtank106@gmail.com>
2023-08-23 01:06:23 +0530

    Update the tips feature blade

--- resources/views/sale_pos/partials/tips_v2_form.blade.php
+++ resources/views/sale_pos/partials/tips_v2_form.blade.php
@@ -1,29 +1,6 @@
 <div class="row">
     <div class="wrapper-of-tips-box">
-
     </div>
-    
-    {{-- <div class="col-md-4 tips_v2_radio" attr-multiplier=".15" data-item="1">
-        <input type="radio" name="tips_v2" id="tips_v2_1" value="0" class="hidden" >
-        <label for="tips_v2_1">
-            <p class="percent">15%</p>
-            <p class="computed"></p>
-        </label>
-    </div>
-    <div class="col-md-4 tips_v2_radio" attr-multiplier=".20" data-item="2">
-        <input type="radio" name="tips_v2" id="tips_v2_2" value="0" class="hidden" >
-        <label for="tips_v2_2">
-            <p class="percent">20%</p>
-            <p class="computed"></p>
-        </label>
-    </div>
-    <div class="col-md-4 tips_v2_radio" attr-multiplier=".25" data-item="3">
-        <input type="radio" name="tips_v2" id="tips_v2_3" value="0" class="hidden">
-        <label for="tips_v2_3">
-            <p class="percent">25%</p>
-            <p class="computed"></p>
-        </label>
-    </div> --}}
     <div class="col-md-12 tips_v2_radio tips_v2_radio_input ct" data-item="4">
         <input type="radio" name="tips_v2" id="tips_v2_4" value="0" class="hidden" >
 
